e973ad9593550a2d533f7eed84abc40725fbff16
Hanz <haniel56@zoho.eu>
2023-12-16 07:37:00 +0530

    chore: change static files path

--- hanz/settings/base.py
+++ hanz/settings/base.py
@@ -155,7 +155,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/5.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
