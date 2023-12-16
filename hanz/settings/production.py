a54ab3f87a78d26e75f943596ea043aff7789228
Hanz <haniel56@zoho.eu>
2023-12-17 01:36:55 +0530

    chore: grab DEBUG from env

--- hanz/settings/production.py
+++ hanz/settings/production.py
@@ -4,7 +4,7 @@ import os
 SECRET_KEY = os.environ.get("SECRET_KEY")
 APP_NAME = os.environ.get("FLY_APP_NAME")
 ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev", os.environ.get("ALLOW_HOST")]
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False")
 
 try:
     from .local import *
