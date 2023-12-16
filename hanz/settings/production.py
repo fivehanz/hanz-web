ae58cb1805a42a99dbb412051fce4b169cc6c6d9
Hanz <haniel56@zoho.eu>
2023-12-17 01:46:47 +0530

    fix: debug and hosts in prod

--- hanz/settings/production.py
+++ hanz/settings/production.py
@@ -3,8 +3,11 @@ import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 APP_NAME = os.environ.get("FLY_APP_NAME")
-ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev", os.environ.get("ALLOW_HOST")]
-DEBUG = os.environ.get("DEBUG", "False")
+ALLOWED_HOSTS = [
+    f"{APP_NAME}.fly.dev",
+    os.environ.get("ALLOW_HOST", f"https://{os.environ.get('ALLOW_HOST')}"),
+]
+DEBUG = os.environ.get("DEBUG", False)
 
 try:
     from .local import *
