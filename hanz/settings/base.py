b7737a21b7dc815ef827771308925b0fd18e5e4b
Hanz <haniel56@zoho.eu>
2023-12-23 03:39:46 +0530

    chore: add admin email for debug

--- hanz/settings/base.py
+++ hanz/settings/base.py
@@ -192,3 +192,7 @@ WAGTAILSEARCH_BACKENDS = {
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = "https://web.hanz.lol"
+
+###
+
+ADMINS = [('hanz', 'haniel56@zoho.eu')]
