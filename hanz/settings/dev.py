1c6ac92edf4242d4d9b9eeaa014a644ab880d121
Hanz <haniel56@zoho.eu>
2023-12-26 14:06:42 +0530

    chore: add defaults env

--- hanz/settings/dev.py
+++ hanz/settings/dev.py
@@ -1,10 +1,11 @@
 from .base import *
+import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b&b1#xb8s&df*ch@*w%k&3_g7vc$of2me#nf6@17jpke4@dhsr"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-b&b1#xb8s&df*ch@*w%k&3_g7vc$of2me#nf6@17jpke4@dhsr")
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
