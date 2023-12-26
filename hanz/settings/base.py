fcf597faeb7a951f01a62c24ab5a561659971492
Hanz <haniel56@zoho.eu>
2023-12-26 12:37:14 +0530

    chore: default db vars

--- hanz/settings/base.py
+++ hanz/settings/base.py
@@ -98,10 +98,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
+        "USER": os.environ.get("DB_USER", "postgres"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", 5432),
     }
 }
 
