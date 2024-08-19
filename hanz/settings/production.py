5f7baeb3f30ebc26f1ca7d4a2ed2e1fc44347b01
Hanz <haniel56@zoho.eu>
2024-08-19 13:41:42 +0530

    fix: rm duplicate config cloudflare cache

--- hanz/settings/production.py
+++ hanz/settings/production.py
@@ -3,14 +3,6 @@ import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-WAGTAILFRONTENDCACHE = {
-    "cloudflare": {
-        "BACKEND": "wagtail.contrib.frontend_cache.backends.CloudflareBackend",
-        "BEARER_TOKEN": os.environ.get("CLOUDFLARE_BEARER_TOKEN"),
-        "ZONEID": os.environ.get("CLOUDFLARE_ZONEID"),
-    },
-}
-
 ALLOWED_HOSTS = os.environ.get("ALLOW_HOSTS", "").split(",")
 CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 
